ee4b9431260fb36eaf45809e1d05f14aa0870cd3
hyj1991 <yeekwanvong@gmail.com>
2020-02-08 14:36:49 +0800

    src: add mock apps data

--- mock/apps/index.js
+++ mock/apps/index.js
@@ -5,7 +5,7 @@ module.exports = app => {
     const type = req.query.type;
     console.log(`get apps type ${type}`);
     const data = require(`./${type}.json`);
-    // setTimeout(() => res.send({ ok: true, data }), 500);
-    res.send({ ok: true, data })
+    setTimeout(() => res.send({ ok: true, data }), 300);
+    // res.send({ ok: true, data })
   });
 };
