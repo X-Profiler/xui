8c3fd9b3f85c62d468914350baeb7f3b02abcb5b
hyj1991 <yeekwanvong@gmail.com>
2020-02-12 15:46:54 +0800

    src: sider layout

--- src/components/instance/Index.vue
+++ src/components/instance/Index.vue
@@ -1,3 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <!-- header -->
+    <x-header active="console"></x-header>
+
+    <div class="x-body">
+      <!-- sider -->
+      <x-sider></x-sider>
+
+      <Content>Content</Content>
+    </div>
+  </div>
 </template>
+
+<script>
+import xHeader from "../layout/Header";
+import xSider from "../layout/Sider";
+export default {
+  components: {
+    "x-header": xHeader,
+    "x-sider": xSider
+  }
+};
+</script>
---
+++ src/components/layout/Sider.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Sider :width="55" class="sider">
+      <div class="menu active">
+        <Icon type="md-albums" class="menu-icon" />
+        <p class="menu-name">实例</p>
+      </div>
+    </Sider>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.sider {
+  min-height: 100vh;
+}
+
+.menu {
+  color: white;
+  height: 70px;
+  padding-top: 10px;
+  cursor: pointer;
+}
+
+.menu-icon {
+  font-size: 22px;
+}
+
+.menu-name {
+  margin-top: 5px;
+}
+
+.active {
+  background-color: #2376b7;
+}
+</style>
