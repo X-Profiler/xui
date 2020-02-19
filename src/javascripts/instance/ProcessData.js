e296a62756704bae4e3ed187e1a91e10f5c31e19
hyj1991 <yeekwanvong@gmail.com>
2020-02-19 17:18:29 +0800

    src: add process line

--- src/javascripts/instance/ProcessData.js
+++ src/javascripts/instance/ProcessData.js
@@ -2,8 +2,11 @@
 
 import { http } from "../config";
 import * as utils from "../lib/utils";
+import * as moment from "moment";
 
 const { xProcesses } = http;
+
+const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export default {
   created() {
@@ -11,15 +14,40 @@ export default {
     this.cancelToken = utils.createCancelToken();
     this.get = utils.get.bind(this);
 
-    // get xprofiler processes
+    this.setLastTime();
     this.getAgentXProcesses();
   },
 
   methods: {
+    setDefaultPid() {
+      if (this.xProcesses.length > 0) {
+        this.selectedPid = this.xProcesses[0].pid;
+      }
+    },
+
     getAgentXProcesses() {
       this.get(xProcesses.msg, xProcesses.url, { appId: this.appId, agentId: this.agentId }, data => {
-        console.log(data);
+        const list = data.list;
+        if (Array.isArray(list)) {
+          this.xProcesses = list;
+          this.setDefaultPid();
+        }
       }, this.cancelToken.token);
+    },
+
+    setLastTime() {
+      const list = [];
+      const today = moment().day();
+      for (let i = 0; i < 24; i += 3) {
+        const time = moment().subtract(i, "hours");
+        const hour = time.hours();
+        if (time.day() !== today && hour === 23) {
+          list.push({ label: time.format("MM.DD"), value: week[time.day()] });
+        } else {
+          list.push({ label: hour < 12 ? "AM" : "PM", value: hour });
+        }
+      }
+      this.times = list;
     }
   }
 };
