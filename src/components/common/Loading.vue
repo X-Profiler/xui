a607c847efdde5da1e00fcb48aefbdbf1a08fc89
hyj1991 <yeekwanvong@gmail.com>
2020-02-19 14:33:06 +0800

    src: add common loading

---
+++ src/components/common/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-show="loading" class="spin-loading" :style="style">
+      <Spin size="large"></Spin>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: Boolean,
+    top: Number
+  },
+
+  computed: {
+    style() {
+      let style = "";
+      if (this.top && !isNaN(this.top)) {
+        style += "margin-top: " + this.top + "px;";
+      }
+      return style;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.spin-loading {
+  display: inline-block;
+  position: relative;
+}
+</style>
