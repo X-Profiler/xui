027aca93ea21008a27c29bee22cfb30bb413d84f
hyj1991 <yeekwanvong@gmail.com>
2020-02-20 21:51:20 +0800

    src: import tooltip in process component

--- src/components/common/Tooltip.vue
+++ src/components/common/Tooltip.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="tooltip">
     <!-- title -->
-    <div class="header">
-      <slot name="header"></slot>
-    </div>
+    <slot name="header"></slot>
 
     <!-- content -->
-    <div class="content">
-      <slot name="content"></slot>
-    </div>
+    <slot name="content"></slot>
 
     <!-- footer -->
     <slot name="footer"></slot>
@@ -26,14 +22,5 @@ export default {};
   z-index: -9999;
   min-width: 300px;
   max-width: 500px;
-}
-
-.header {
-  height: 20px;
-  background-color: blue;
-}
-
-.content {
-  padding: 10px 0;
 }
 </style>
