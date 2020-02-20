b5beb7421a665bfbab799ca292bae269ffaed141
hyj1991 <yeekwanvong@gmail.com>
2020-02-20 21:43:50 +0800

    src: add common tooltip component

---
+++ src/components/common/Tooltip.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tooltip">
+    <!-- title -->
+    <div class="header">
+      <slot name="header"></slot>
+    </div>
+
+    <!-- content -->
+    <div class="content">
+      <slot name="content"></slot>
+    </div>
+
+    <!-- footer -->
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.tooltip {
+  background: #f8f8f9;
+  position: absolute;
+  z-index: -9999;
+  min-width: 300px;
+  max-width: 500px;
+}
+
+.header {
+  height: 20px;
+  background-color: blue;
+}
+
+.content {
+  padding: 10px 0;
+}
+</style>
