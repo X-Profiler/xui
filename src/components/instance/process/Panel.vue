2344eb553591f8c10f5f8966cde71d06b3689491
hyj1991 <yeekwanvong@gmail.com>
2020-02-22 17:23:23 +0800

    src: add panel title

--- src/components/instance/process/Panel.vue
+++ src/components/instance/process/Panel.vue
@@ -6,6 +6,14 @@
         <Icon type="md-apps" class="check-button-icon" />查看运行中的 Node.js 进程
       </Button>
     </div>
+
+    <!-- process panel -->
+    <div class="panel">
+      <div class="panel-title" :style="panelStyle">
+        <div class="panel-title-pid">PID: {{ processData.pid }}</div>
+        <div class="panel-title-cmd" :title="processData.cmd">{{ processData.cmd }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,8 +22,12 @@ import panelModule from "../../../javascripts/instance/process/Panel";
 
 const panelData = Object.assign(
   {
-    props: {
-      processes: Array
+    data() {
+      return {
+        processData: {
+          pid: "未知"
+        }
+      };
     }
   },
   panelModule
@@ -31,5 +43,31 @@ export default panelData;
 
 .check-button-icon {
   margin-right: 5px;
+}
+
+.panel {
+  margin-top: 5px;
+}
+
+.panel-title {
+  height: 65px;
+  background-color: #c5c8ce;
+  text-align: left;
+  padding: 10px 15px;
+  color: #fff;
+}
+
+.panel-title-pid {
+  font-size: 17px;
+  font-weight: bold;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.panel-title-cmd {
+  margin-top: 3px;
+  font-size: 12px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 </style>
--- src/javascripts/instance/process/Panel.js
+++ src/javascripts/instance/process/Panel.js
@@ -1,5 +1,23 @@
 "use strict";
 
 export default {
+  methods: {
+    updateSelectedProcess(data) {
+      this.processData = data;
+    }
+  },
 
+  computed: {
+    panelStyle() {
+      const processData = this.processData;
+      let style = "";
+
+      // add color
+      if (processData.color) {
+        style += "background-color: " + processData.color + ";"
+      }
+
+      return style;
+    }
+  }
 };
