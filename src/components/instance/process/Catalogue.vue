08ea09612f92e39eaef304aded72e79b6692e297
hyj1991 <yeekwanvong@gmail.com>
2020-02-26 14:53:48 +0800

    src: add instance catalogue dropdown

--- src/components/instance/process/Catalogue.vue
+++ src/components/instance/process/Catalogue.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="catalogue">
     <div style="position: relative" v-for="(catg, index) in catalogues" :key="index">
-      <div class="tag dropdown-list">{{ catg.label }}</div>
+      <div class="tag dropdown-list">
+        <x-dropdown class="panel-title-dropdown" :title="catg.label"></x-dropdown>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import xDropdown from "../../common/Dropdown";
 import catalogueModule from "../../../javascripts/instance/process/Catalogue";
 
 const catalogueData = Object.assign(
   {
+    components: {
+      "x-dropdown": xDropdown
+    },
     data() {
       return {
         catalogues: [
