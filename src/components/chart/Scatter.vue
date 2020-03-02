aa13db961e4d2dece28660d4086b19843b0b2835
hyj1991 <yeekwanvong@gmail.com>
2020-03-02 10:24:43 +0800

    src: optimize scatter view

--- src/components/chart/Scatter.vue
+++ src/components/chart/Scatter.vue
@@ -1,92 +1,99 @@
 <template>
   <div ref="content">
-    <svg width="100%" :height="viewHeight" :viewBox="`0, 0, ${viewWidth}, ${viewHeight}`">
-      <!-- chart axis -->
-      <g>
-        <!-- x axis -->
-        <line
-          class="axis"
-          :x1="paddingLeft"
-          :y1="viewHeight - paddingBottom"
-          :x2="viewWidth - paddingRight"
-          :y2="viewHeight - paddingBottom"
-        />
-
-        <!-- y axis -->
-        <line
-          class="axis"
-          :x1="paddingLeft"
-          :y1="paddingTop"
-          :x2="paddingLeft"
-          :y2="viewHeight - paddingBottom"
-        />
-
-        <!-- y grid -->
-        <g v-for="(yAxis, index) in yAxisScale" :key="index">
-          <text
-            class="axisScale"
-            style="text-anchor: end;"
-            :x="paddingLeft"
-            :y="paddingTop + (viewHeight - paddingTop - paddingBottom ) / yAxisScaleCountInner * index"
-            dx="-0.5em"
-            dy="0.32em"
-          >{{ yAxis }}</text>
+    <transition name="slide-rightward">
+      <svg
+        v-show="data.length"
+        width="100%"
+        :height="viewHeight"
+        :viewBox="`0, 0, ${viewWidth}, ${viewHeight}`"
+      >
+        <!-- chart axis -->
+        <g>
+          <!-- x axis -->
           <line
             class="axis"
             :x1="paddingLeft"
-            :y1="paddingTop + (viewHeight - paddingTop - paddingBottom ) / yAxisScaleCountInner * index"
+            :y1="viewHeight - paddingBottom"
             :x2="viewWidth - paddingRight"
-            :y2="paddingTop + (viewHeight - paddingTop - paddingBottom ) / yAxisScaleCountInner * index"
+            :y2="viewHeight - paddingBottom"
+          />
+
+          <!-- y axis -->
+          <line
+            class="axis"
+            :x1="paddingLeft"
+            :y1="paddingTop"
+            :x2="paddingLeft"
+            :y2="viewHeight - paddingBottom"
+          />
+
+          <!-- y grid -->
+          <g v-for="(yAxis, index) in yAxisScale" :key="index">
+            <text
+              class="axisScale"
+              style="text-anchor: end;"
+              :x="paddingLeft"
+              :y="paddingTop + (viewHeight - paddingTop - paddingBottom ) / yAxisScaleCountInner * index"
+              dx="-0.5em"
+              dy="0.32em"
+            >{{ yAxis }}</text>
+            <line
+              class="axis"
+              :x1="paddingLeft"
+              :y1="paddingTop + (viewHeight - paddingTop - paddingBottom ) / yAxisScaleCountInner * index"
+              :x2="viewWidth - paddingRight"
+              :y2="paddingTop + (viewHeight - paddingTop - paddingBottom ) / yAxisScaleCountInner * index"
+            />
+          </g>
+        </g>
+
+        <!-- chart xAxis scale  -->
+        <g>
+          <text
+            v-for="(xAxis, index) in xAxisScale"
+            :key="index"
+            class="axisScale"
+            style="text-anchor: middle;"
+            :x="paddingLeft + (viewWidth -paddingLeft- paddingRight) / xAxisScaleCountInner * index"
+            :y="viewHeight-paddingBottom"
+            dy="1.4em"
+          >{{ xAxis }}</text>
+        </g>
+
+        <!-- scale unit -->
+        <g>
+          <!-- x axis -->
+          <text
+            :x="viewWidth - paddingRight"
+            :y="viewHeight-paddingBottom"
+            :dx="xAxisUnit[1]"
+            dy="-0.7em"
+            class="axisUnit"
+          >{{ fields[0] }}/{{ xAxisUnit[0] }}</text>
+
+          <!-- y axis -->
+          <text
+            :x="paddingLeft"
+            :y="paddingTop"
+            :dx="yAxisUnit[1]"
+            dy="-0.7em"
+            class="axisUnit"
+          >{{ fields[1] }}/{{ yAxisUnit[0] }}</text>
+        </g>
+
+        <!-- scatter dot -->
+        <g v-for="(info, index) in data" :key="index">
+          <circle
+            :cx="getCx(info)"
+            :cy="getCy(info)"
+            class
+            r="6"
+            :stroke="info.color"
+            :fill="info.color"
           />
         </g>
-      </g>
-
-      <!-- chart xAxis scale  -->
-      <g>
-        <text
-          v-for="(xAxis, index) in xAxisScale"
-          :key="index"
-          class="axisScale"
-          style="text-anchor: middle;"
-          :x="paddingLeft + (viewWidth -paddingLeft- paddingRight) / xAxisScaleCountInner * index"
-          :y="viewHeight-paddingBottom"
-          dy="1.4em"
-        >{{ xAxis }}</text>
-      </g>
-
-      <!-- scale unit -->
-      <g>
-        <!-- x axis -->
-        <text
-          :x="viewWidth - paddingRight"
-          :y="viewHeight-paddingBottom"
-          :dx="xAxisUnit[1]"
-          dy="-0.7em"
-          class="axisUnit"
-        >{{ fields[0] }}/{{ xAxisUnit[0] }}</text>
-
-        <!-- y axis -->
-        <text
-          :x="paddingLeft"
-          :y="paddingTop"
-          :dx="yAxisUnit[1]"
-          dy="-0.7em"
-          class="axisUnit"
-        >{{ fields[1] }}/{{ yAxisUnit[0] }}</text>
-      </g>
-
-      <!-- scatter dot -->
-      <g v-for="(info, index) in data" :key="index">
-        <circle
-          :cx="getCx(info)"
-          :cy="getCy(info)"
-          class="selected"
-          r="6"
-          :stroke="info.color"
-          :fill="info.color"
-        />
-      </g>
-    </svg>
+      </svg>
+    </transition>
   </div>
 </template>
 
@@ -116,6 +123,7 @@ export default {
 
   mounted() {
     this.content = this.$refs.content;
+    this.setViewBox();
     window.addEventListener("resize", this.setViewBox.bind(this));
   },
 
