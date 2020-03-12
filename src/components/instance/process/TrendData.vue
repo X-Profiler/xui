6654fdc437deb9433737ce2d609cf68db0b98108
hyj1991 <yeekwanvong@gmail.com>
2020-03-12 21:47:08 +0800

    src: add chart label

--- src/components/instance/process/TrendData.vue
+++ src/components/instance/process/TrendData.vue
@@ -9,16 +9,24 @@
 
       <x-error-message v-if="loadError" :message="loadError" top="100"></x-error-message>
 
-      <x-area
-        v-if="!loading && !loadError"
-        xAxis="time"
-        ref="area"
-        :yAxis="yAxis"
-        :data="areaData"
-        :noDataText="noDataText"
-        @linkage="linkage"
-        @hidden="hidden"
-      ></x-area>
+      <div v-if="!loading && !loadError">
+        <x-area
+          xAxis="time"
+          ref="area"
+          :yAxis="yAxis"
+          :data="areaData"
+          :noDataText="noDataText"
+          @linkage="linkage"
+          @hidden="hidden"
+        ></x-area>
+
+        <div class="chart-label">
+          <div class="chart-label-group" v-for="(axis, index) in yAxis" :key="index">
+            <div class="label-icon"></div>
+            <div class="label-value">{{axis}}</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -51,5 +59,30 @@ export default trendData;
 .title {
   font-size: 15px;
   font-weight: bold;
+}
+
+.chart-label {
+  margin-top: 5px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.chart-label-group {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.label-icon {
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  background-color: black;
+}
+
+.label-value {
+  margin: 0 25px 0 5px;
 }
 </style>
