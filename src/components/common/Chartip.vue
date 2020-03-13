b2b8eed127c76156c1aca23e5c56825bbef8c3fc
hyj1991 <yeekwanvong@gmail.com>
2020-03-13 22:04:21 +0800

    src: add chartip style

--- src/components/common/Chartip.vue
+++ src/components/common/Chartip.vue
@@ -1,5 +1,14 @@
 <template>
-  <div ref="tooltip"></div>
+  <div ref="tooltip" class="tooltip box-shadow">
+    <!-- title -->
+    <slot name="header"></slot>
+
+    <!-- content -->
+    <slot name="content"></slot>
+
+    <!-- footer -->
+    <slot name="footer"></slot>
+  </div>
 </template>
 
 <script>
@@ -7,4 +16,14 @@ export default {};
 </script>
 
 <style scoped>
+.tooltip {
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: -9999;
+  min-width: 300px;
+  max-width: 500px;
+  display: none;
+}
 </style>
