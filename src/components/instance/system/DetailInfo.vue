cadab19862e8c02460cb3fbcec556ac16de56dac
hyj1991 <yeekwanvong@gmail.com>
2020-03-21 14:24:56 +0800

    src: init system overview & detail info

---
+++ src/components/instance/system/DetailInfo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>system detail</div>
+</template>
+
+<script>
+import detailInfoModule from "../../../javascripts/instance/system/DetailInfo";
+
+export default {
+  ...detailInfoModule
+};
+</script>
--- src/components/instance/system/SystemIndex.vue
+++ src/components/instance/system/SystemIndex.vue
@@ -2,18 +2,27 @@
   <div>
     <div class="overview">
       <div class="process-body-title">系统信息概览</div>
+      <x-overview class="overview-content"></x-overview>
     </div>
 
     <div class="detail">
       <div class="process-body-title">系统信息周期详情</div>
+      <x-detail-info class="detail-content"></x-detail-info>
     </div>
   </div>
 </template>
 
 <script>
 import systemModule from "../../../javascripts/instance/system/SystemIndex";
+import overview from "./Overview";
+import detailInfo from "./DetailInfo";
 
 export default {
+  components: {
+    "x-overview": overview,
+    "x-detail-info": detailInfo
+  },
+
   ...systemModule
 };
 </script>
@@ -21,6 +30,11 @@ export default {
 <style scoped>
 .detail {
   margin-top: 15px;
+}
+
+.overview-content,
+.detail-content {
+  margin-top: 10px;
 }
 
 .process-body-title {
