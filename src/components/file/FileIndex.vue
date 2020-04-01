b3d486526344cfb8f13bc5a6d45fb96b516f2d75
hyj1991 <yeekwanvong@gmail.com>
2020-04-01 14:54:50 +0800

    src: add file upload button

--- src/components/file/FileIndex.vue
+++ src/components/file/FileIndex.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="detail-content">
     <!-- title -->
-    <x-dashboard-title :appName="appName" :dashboardTitle="title" line></x-dashboard-title>
+    <x-dashboard-title :appName="appName" :dashboardTitle="title" line>
+      <x-upload slot="extra"></x-upload>
+    </x-dashboard-title>
   </div>
 </template>
 
 <script>
+import fileModule from "@/javascripts/file/FileIndex";
+import xUpload from "@/components/file/Upload";
+
 export default {
   props: {
     appName: String,
     title: String,
     currentUserIsOwner: Boolean
-  }
+  },
+
+  components: {
+    "x-upload": xUpload
+  },
+
+  ...fileModule
 };
 </script>
---
+++ src/components/file/Upload.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Button class="upload-button" type="info" size="small">
+      <div>
+        <Icon class="upload-icon" type="md-cloud-upload" />
+        <span class="upload-text">上传文件</span>
+      </div>
+    </Button>
+  </div>
+</template>
+
+<script>
+import uploadModule from "@/javascripts/file/Upload";
+
+export default {
+  ...uploadModule
+};
+</script>
+
+<style scoped>
+.upload-button {
+  margin-left: 20px;
+}
+
+.upload-icon {
+  font-size: 13px;
+}
+
+.upload-text {
+  font-size: 11px;
+  margin-left: 6px;
+}
+</style>
