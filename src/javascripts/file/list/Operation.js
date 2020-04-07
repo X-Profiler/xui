41eacab5a3ab5b5f8341bd931bff3ba8e708f5d5
hyj1991 <yeekwanvong@gmail.com>
2020-04-07 10:05:06 +0800

    src: complete total button

--- src/javascripts/file/list/Operation.js
+++ src/javascripts/file/list/Operation.js
@@ -6,13 +6,25 @@ export default {
   },
 
   methods: {
-    createButton(label, bt, icon, loading = false, disabled = false, color) {
+    getColor(bt) {
+      let color = this.disableColor;
       if (bt === "success") {
         color = this.successColor;
       }
+      if (bt === "info") {
+        color = this.infoColor;
+      }
+      if (bt === "warning") {
+        color = this.warningColor;
+      }
+      return color;
+    },
+
+    createButton(label, bt, icon, loading = false, disabled = false) {
       return {
         type: "button",
-        label, bt, icon, loading, disabled, color
+        label, bt, icon, loading, disabled,
+        color: this.getColor(bt),
       };
     },
 
@@ -20,30 +32,42 @@ export default {
       return { type: "line", color };
     },
 
-    loadingButton(label, bt) {
-      return this.createButton(label, bt, undefined, true, false);
-    },
-
-    disableButton(label, icon) {
-      return this.createButton(label, undefined, icon, false, true, this.disableColor);
-    },
-
     doneButton(label, icon, bt) {
       return this.createButton(label, bt, icon, false, false);
+    },
+
+    createLoadingGroup(label, bt) {
+      return [
+        this.createLine(this.getColor(bt)),
+        this.createButton(label, bt, undefined, true, false)
+      ];
     },
 
     createDisableGroup(label, icon) {
       return [
         this.createLine(this.disableColor),
-        this.createButton(label, undefined, icon, false, true, this.disableColor)
-      ]
+        this.createButton(label, undefined, icon, false, true)
+      ];
     },
 
     createDoneGroup(label, icon, bt) {
       return [
-        this.createLine(this.successColor),
+        this.createLine(this.getColor(bt)),
         this.doneButton(label, icon, bt)
-      ]
+      ];
+    },
+
+    createLeftGroup(fileType) {
+      const operations = [];
+      if (this.devtools.includes(fileType)) {
+        operations.push(...this.createDisableGroup("devtools", "md-search"));
+      }
+      if (this.xprofiler.includes(fileType)) {
+        operations.push(...this.createDisableGroup("xprofiler", "md-search"));
+      }
+      operations.push(...this.createDisableGroup("下载", "md-cloud-download"));
+      operations.push(...this.createDisableGroup("收藏", "md-star"));
+      return operations;
     },
 
     updateOperation() {
@@ -53,30 +77,37 @@ export default {
 
       // file creating
       if (status === 0) {
-        operations.push(this.loadingButton("生成中"));
+        operations.push(this.createButton("生成中", undefined, undefined, true, true));
         operations.push(...this.createDisableGroup("转储", "md-cloud-upload"));
-        if (this.devtools.includes(data.fileType)) {
-          operations.push(...this.createDisableGroup("devtools", "md-search"));
-        }
-        if (this.xprofiler.includes(data.fileType)) {
-          operations.push(...this.createDisableGroup("xprofiler", "md-search"));
-        }
-        operations.push(...this.createDisableGroup("下载", "md-cloud-download"));
-        operations.push(...this.createDisableGroup("收藏", "md-star"));
+        operations.push(...this.createLeftGroup(data.fileType));
       }
 
       // file created
       if (status === 1) {
         operations.push(this.doneButton("已生成", "md-brush", "success"));
         operations.push(...this.createDoneGroup("转储", "md-cloud-upload", "success"));
+        operations.push(...this.createLeftGroup(data.fileType));
+      }
+
+      // file transferring
+      if (status === 2) {
+        operations.push(this.doneButton("已生成", "md-brush", "success"));
+        operations.push(...this.createLoadingGroup("转储中", "success"));
+        operations.push(...this.createLeftGroup(data.fileType));
+      }
+
+      // file transferred
+      if (status === 3) {
+        operations.push(this.doneButton("已生成", "md-brush", "success"));
+        operations.push(...this.createDoneGroup("再转储", "md-cloud-upload", "success"));
         if (this.devtools.includes(data.fileType)) {
-          operations.push(...this.createDisableGroup("devtools", "md-search"));
+          operations.push(...this.createDoneGroup("devtools", "md-search", "info"));
         }
         if (this.xprofiler.includes(data.fileType)) {
-          operations.push(...this.createDisableGroup("xprofiler", "md-search"));
+          operations.push(...this.createDoneGroup("xprofiler", "md-search", "info"));
         }
-        operations.push(...this.createDisableGroup("下载", "md-cloud-download"));
-        operations.push(...this.createDisableGroup("收藏", "md-star"));
+        operations.push(...this.createDoneGroup("下载", "md-cloud-download", "info"));
+        operations.push(...this.createDoneGroup("收藏", "md-star", "warning"));
       }
 
       this.operations = operations;
