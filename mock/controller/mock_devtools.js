9314661ba3e848f582613c2c586dae33aa51e842
hyj1991 <yeekwanvong@gmail.com>
2020-04-11 15:44:58 +0800

    src: add old devtools

--- mock/controller/mock_devtools.js
+++ mock/controller/mock_devtools.js
@@ -10,4 +10,11 @@ module.exports = app => {
     query.fileName = path.basename(query.fileName);
     res.redirect(`/dashboard/devtools-new/devtools_app.html?${qs.stringify(req.query)}`);
   });
+
+  //  the old devtools
+  app.get('/dashboard/devtools-old', function (req, res) {
+    const query = req.query;
+    query.fileName = path.basename(query.fileName);
+    res.redirect(`/dashboard/devtools-old/devtools_app.html?${qs.stringify(req.query)}`);
+  });
 };
