c528ceef48323c5166a7e144f73b3ff15038f443
hyj1991 <yeekwanvong@gmail.com>
2020-04-16 09:37:37 +0800

    src: add javascript stacks

--- src/components/analytics/diag/display/JavaScript.vue
+++ src/components/analytics/diag/display/JavaScript.vue
@@ -3,10 +3,41 @@
     <!-- heap statistics -->
     <div class="section-title">进程堆信息</div>
 
-    <x-heap style="margin-top: 5px;"></x-heap>
+    <x-heap></x-heap>
 
     <!-- javascripts stacks -->
-    <div class="section-title" style="margin-top: 20px;">函数调用栈</div>
+    <div class="section-title" style="margin-top: 25px;">函数调用栈</div>
+
+    <x-table
+      class="js-stack"
+      style="margin-top: 15px;"
+      :columns="columns"
+      :data="jsStacks"
+      noDataText="暂无 JavaScript 栈信息"
+      no-data-head
+      stribe
+    >
+      <!-- pc address -->
+      <template v-slot:pcAddress="{ row }">
+        <div class="padding">
+          <code>{{ row.pcAddress }}</code>
+        </div>
+      </template>
+
+      <!-- frame type -->
+      <template v-slot:frameType="{ row }">
+        <div class="padding">
+          <code>{{ row.frameType }}</code>
+        </div>
+      </template>
+
+      <!-- frame detail -->
+      <template v-slot:frameDetail="{ row }">
+        <div class="padding">
+          <code>{{ row.frameDetail }}</code>
+        </div>
+      </template>
+    </x-table>
   </div>
 </template>
 
@@ -19,6 +50,34 @@ export default {
     "x-heap": xHeap
   },
 
+  data() {
+    return {
+      columns: [
+        {
+          title: "寄存器指令地址",
+          value: "pcAddress",
+          width: 140
+        },
+        {
+          title: "函数类型",
+          value: "frameType",
+          width: 135,
+          align: "center"
+        },
+        {
+          title: "执行栈帧",
+          value: "frameDetail"
+        }
+      ]
+    };
+  },
+
   ...javascriptModule
 };
 </script>
+
+<style scoped>
+.padding {
+  padding: 5px 0;
+}
+</style>
