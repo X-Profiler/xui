5593bb79dd6735ca5574d4da6d3c0c529eab5455
hyj1991 <yeekwanvong@gmail.com>
2020-04-16 09:37:37 +0800

    src: add javascript stacks

--- src/javascripts/analytics/diag/display/JavaScript.js
+++ src/javascripts/analytics/diag/display/JavaScript.js
@@ -1,4 +1,27 @@
 "use strict";
 
+import * as utils from "@/javascripts/lib/utils";
+
+const { mapState } = utils.createNamespace("dashboard/analytics");
+
 export default {
+  computed: {
+    ...mapState(["file_data"]),
+
+    jsStacks() {
+      const { jsStacks } = this.file_data;
+
+      return jsStacks.map(frame => {
+        const { pcAddress, frameType,
+          functionName, scriptName, lineNumber, column } = frame;
+        const frameDetail = `${functionName}() (${scriptName}:${lineNumber}:${column})`;
+
+        return {
+          pcAddress,
+          frameType,
+          frameDetail
+        };
+      });
+    }
+  }
 };
