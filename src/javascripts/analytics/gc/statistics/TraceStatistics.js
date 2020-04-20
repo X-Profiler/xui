2121bc827503928fa9198498853429ba45db7cb8
hyj1991 <yeekwanvong@gmail.com>
2020-04-20 10:45:55 +0800

    src: add gc trace analytics

--- src/javascripts/analytics/gc/statistics/TraceStatistics.js
+++ src/javascripts/analytics/gc/statistics/TraceStatistics.js
@@ -1,5 +1,41 @@
 "use strict";
 
-export default {
+import * as utils from "@/javascripts/lib/utils";
+const { mapState } = utils.createNamespace("dashboard/analytics");
 
+export default {
+  methods: {
+    formatPercentage(pect) {
+      return pect.toFixed(2);
+    }
+  },
+
+  computed: {
+    ...mapState(["file_data"]),
+
+    startTime() {
+      const { startTime = 0 } = this.file_data;
+      return startTime * 1000;
+    },
+
+    stopTime() {
+      const { stopTime = 0 } = this.file_data;
+      return stopTime * 1000;
+    },
+
+    totalPauseTime() {
+      const { gc: gcList } = this.file_data;
+      let total = 0;
+      for (const gc of gcList) {
+        total += +(gc.end - gc.start);
+      }
+      return total;
+    },
+
+    gcOccupy() {
+      const pause = this.totalPauseTime;
+      const total = this.stopTime - this.startTime;
+      return pause / total * 100;
+    }
+  }
 };
