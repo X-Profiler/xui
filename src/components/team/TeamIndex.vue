0ed6138273d0c0c00fe2500f4003695bdccedb57
hyj1991 <yeekwanvong@gmail.com>
2020-04-27 10:34:56 +0800

    src: add team tips

--- src/components/team/TeamIndex.vue
+++ src/components/team/TeamIndex.vue
@@ -2,6 +2,16 @@
   <div class="detail-content">
     <!-- title -->
     <x-dashboard-title :appName="appName" :dashboardTitle="title" line></x-dashboard-title>
+
+    <!-- message -->
+    <Alert class="x-alert tip" type="info">
+      <div class="alert-group">
+        <Icon style="color: #2376b7;`" type="ios-alert-outline" />
+        <div
+          class="alert-desc"
+        >您可以将应用授权给其它成员进行管理，团队成员可以查看完整的应用监控数据，也可以访问生成的性能分析文件，方便协作定位 Node.js 应用故障</div>
+      </div>
+    </Alert>
   </div>
 </template>
 
@@ -14,3 +24,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.tip {
+  margin-top: 15px;
+}
+</style>
