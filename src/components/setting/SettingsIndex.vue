f2a8b702963ab411d15576c245e5bfbfb2376df7
hyj1991 <yeekwanvong@gmail.com>
2020-05-05 16:30:43 +0800

    src: optimize settings transition

--- src/components/setting/SettingsIndex.vue
+++ src/components/setting/SettingsIndex.vue
@@ -14,7 +14,9 @@
     ></x-error-message>
 
     <!-- show settings -->
-    <x-settings-content v-if="!settings_loading && !settings_load_error"></x-settings-content>
+    <transition name="slide-noward">
+      <x-settings-content v-if="!settings_loading && !settings_load_error"></x-settings-content>
+    </transition>
 
     <!-- modify app name -->
     <x-modal ref="modify" title="修改应用名称" :padding="0" @canceled="closeModifyModal" hide-footer>
@@ -22,7 +24,14 @@
     </x-modal>
 
     <!-- delete app -->
-    <x-modal ref="delete" title="删除应用" :padding="0" @canceled="closeDeleteModal" :width="650" hide-footer>
+    <x-modal
+      ref="delete"
+      title="删除应用"
+      :padding="0"
+      @canceled="closeDeleteModal"
+      :width="650"
+      hide-footer
+    >
       <x-app-delete slot="content"></x-app-delete>
     </x-modal>
   </div>
