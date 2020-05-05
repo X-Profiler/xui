032ee0488296cebc343f0477a531d341247097e4
hyj1991 <yeekwanvong@gmail.com>
2020-05-05 15:46:19 +0800

    src: optimize error logs transition

--- src/components/instance/errors/ErrorLogs.vue
+++ src/components/instance/errors/ErrorLogs.vue
@@ -1,48 +1,57 @@
 <template>
-  <div v-show="!logs_loading && !logs_load_error">
-    <x-table :columns="columns" :data="errors" noDataText="当前应用暂无错误信息" no-data-head border stribe2>
-      <!-- occured time -->
-      <template v-slot:occuredTime="{ row }">
-        <div class="error-content">
-          <code>{{ row.date}}</code>
-        </div>
-        <div class="error-content" style="margin-top:5px;">
-          <code>{{ row.time}}</code>
-        </div>
-      </template>
-
-      <!-- error type -->
-      <template v-slot:errorType="{ row }">
-        <code class="error-content">{{ row.type }}</code>
-      </template>
-
-      <!-- error message -->
-      <template v-slot:errorDetail="{ row }">
-        <div class="error-content">
-          <div v-for="(line, index) in row.stack" :key="index">
-            <code v-if="index === 0">{{ line }}</code>
-            <code v-else style="margin-left: 15px;">{{ line }}</code>
+  <transition name="slide-noward">
+    <div v-show="!logs_loading && !logs_load_error">
+      <x-table
+        :columns="columns"
+        :data="errors"
+        noDataText="当前应用暂无错误信息"
+        no-data-head
+        border
+        stribe2
+      >
+        <!-- occured time -->
+        <template v-slot:occuredTime="{ row }">
+          <div class="error-content">
+            <code>{{ row.date}}</code>
           </div>
-          <div v-if="row.extra.length">
-            <div v-for="(line, index) in row.extra" :key="index">
-              <code>{{ line }}</code>
+          <div class="error-content" style="margin-top:5px;">
+            <code>{{ row.time}}</code>
+          </div>
+        </template>
+
+        <!-- error type -->
+        <template v-slot:errorType="{ row }">
+          <code class="error-content">{{ row.type }}</code>
+        </template>
+
+        <!-- error message -->
+        <template v-slot:errorDetail="{ row }">
+          <div class="error-content">
+            <div v-for="(line, index) in row.stack" :key="index">
+              <code v-if="index === 0">{{ line }}</code>
+              <code v-else style="margin-left: 15px;">{{ line }}</code>
+            </div>
+            <div v-if="row.extra.length">
+              <div v-for="(line, index) in row.extra" :key="index">
+                <code>{{ line }}</code>
+              </div>
             </div>
           </div>
-        </div>
-      </template>
-    </x-table>
+        </template>
+      </x-table>
 
-    <div v-if="totaLogCount" class="pagination">
-      <Page
-        :total="totaLogCount"
-        :page-size="pageSize"
-        :current="currentPage"
-        size="small"
-        show-elevator
-        @on-change="changeLogPage"
-      />
+      <div v-if="totaLogCount" class="pagination">
+        <Page
+          :total="totaLogCount"
+          :page-size="pageSize"
+          :current="currentPage"
+          size="small"
+          show-elevator
+          @on-change="changeLogPage"
+        />
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
