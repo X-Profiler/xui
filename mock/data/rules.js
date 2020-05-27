78d0fabbe16b43931eaa567641e2d58a845515d6
hyj1991 <yeekwanvong@gmail.com>
2020-05-27 14:57:29 +0800

    src: show disable alarm

--- mock/data/rules.js
+++ mock/data/rules.js
@@ -2,12 +2,16 @@
 
 module.exports = [
   {
-    strategyId: 1,
+    strategyId: 8,
     contextType: 'xprofiler_log',
     pushType: 'p3',
-    expression: '@cpu_60 > 90',
-    alarmContent: '1 分钟内 CPU 使用率超过 90%，当前为 ${@cpu_60}%',
+    expression: '@cpu_60 > 80',
+    alarmContent: '1 分钟内 CPU 使用率超过 80%，当前为 ${@cpu_60}%',
     status: 1,
+    webhookPush: 0,
+    webhookType: "dingtalk",
+    webhookAddress: '',
+    webhookSign: '',
     alarmCount: 666
   },
   {
@@ -20,12 +24,17 @@ module.exports = [
     alarmCount: 0
   },
   {
-    strategyId: 3,
+
+    strategyId: 7,
     contextType: 'system_log',
     pushType: 'p3',
     expression: '@disk_usage > 85',
     alarmContent: '磁盘 (${@mounted_on}) 占比超过 85%：为 ${@disk_usage}%',
     status: 1,
+    webhookPush: true,
+    webhookType: 'dingtalk',
+    webhookAddress: 'https://oapi.dingtalk.com/robot/send?access_token=458c8b0d53ac27f3ff40de3ac460034e6cbf28339cf4065fd680463c49b9851b',
+    webhookSign: 'SECdea**********************************************************',
     alarmCount: 520
   }
 ];
