86a252ac38e5c3e5e9a8d42f7dea26db0c6c2137
hyj1991 <yeekwanvong@gmail.com>
2021-03-12 11:26:27 +0800

    src: remove download file

--- src/components/analytics/snapshot/SnapshotIndex.vue
+++ src/components/analytics/snapshot/SnapshotIndex.vue
@@ -9,7 +9,7 @@
     <div class="snapshot-content">
       <div class="snapshot-loading">
         <x-loading
-          :loading="file_loading"
+          :loading="loading"
           top="calc(40vh - 50px)"
           type="dot"
           size="large"
@@ -17,17 +17,13 @@
       </div>
 
       <x-error-message
-        v-show="file_load_error"
-        :message="file_load_error"
+        v-show="error"
+        :message="error"
         top="calc(40vh - 57px)"
       ></x-error-message>
 
       <transition name="slide-noward">
-        <div v-if="!file_loading && !file_load_error" class="snapshot-detail">
-          <!-- snapshot -->
-          <!-- <x-flamegraph :snapshotData="customData.flamegraph"></x-flamegraph> -->
-          {{ file_data }}
-        </div>
+        <div v-if="!loading && !error" class="snapshot-detail"></div>
       </transition>
     </div>
   </div>
@@ -37,6 +33,13 @@
 import snapshotModule from "@/javascripts/analytics/snapshot/SnapshotIndex";
 
 export default {
+  data() {
+    return {
+      loading: true,
+      error: null,
+    };
+  },
+
   ...snapshotModule,
 };
 </script>
--- src/javascripts/analytics/snapshot/SnapshotIndex.js
+++ src/javascripts/analytics/snapshot/SnapshotIndex.js
@@ -3,29 +3,18 @@
 import * as utils from "@/javascripts/lib/utils";
 
 const { mapState } = utils.createNamespace("dashboard/file/wrapper");
-const { mapState: mapStateAnalytics, mapActions: mapActionsAnalytics } = utils.createNamespace("dashboard/analytics");
 
 export default {
   created() {
     this.cancelToken = utils.createCancelToken();
-    this.downloadFile({
-      cancelToken: this.cancelToken.token,
-      fileId: this.snapshotData.fileId,
-      fileType: this.snapshotData.fileType
-    });
+    console.log(this.snapshotData);
   },
 
   beforeDestroy() {
     utils.cancelRequest(this.cancelToken);
   },
 
-  methods: {
-    ...mapActionsAnalytics(["downloadFile"])
-  },
-
   computed: {
     ...mapState(["snapshotData"]),
-
-    ...mapStateAnalytics(["file_loading", "file_load_error", "file_data"]),
   }
 };
