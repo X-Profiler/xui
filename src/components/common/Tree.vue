3c79277cc21bf778ec518d26ceacb90f1e9a6ae3
hyj1991 <yeekwanvong@gmail.com>
2021-04-21 10:11:57 +0800

    src: add tree icon

--- src/components/common/Tree.vue
+++ src/components/common/Tree.vue
@@ -7,8 +7,19 @@
       :key="index"
       @click.stop="toggle(child)"
     >
-      <div v-html="child.title"></div>
+      <div class="title">
+        <div class="icon-translate">
+          <Icon
+            :ref="`dropIcon-${child.id}`"
+            class="icon-rotate"
+            :style="iconExpandStyle(child)"
+            type="md-arrow-dropright"
+          />
+        </div>
+        <div v-html="child.title"></div>
+      </div>
 
+      <!-- <transition name="slide-noward"> -->
       <div v-if="child.expand">
         <x-tree
           :data="child.children"
@@ -23,6 +34,7 @@
           +{{ child.left }}
         </div>
       </div>
+      <!-- </transition> -->
     </div>
   </div>
 </template>
@@ -41,14 +53,36 @@ export default {
     },
   },
 
+  data() {
+    return {
+      inconDownStyle:
+        "scaleX(1.2) scaleY(1.4) translateY(-1px) translateX(-2px) rotate(90deg)",
+    };
+  },
+
   methods: {
     toggle(tree) {
       this.$set(tree, "expand", !tree.expand);
       this.expandNode(tree);
+
+      const dropIcon = this.$refs[`dropIcon-${tree.id}`][0];
+      if (!dropIcon) {
+        return;
+      }
+      if (tree.expand) {
+        dropIcon.$el.style.transform = this.inconDownStyle;
+        dropIcon.$el.style.transition = "transform 0.1s ease-out";
+      } else {
+        dropIcon.$el.style.transform = "scaleY(1.2) scaleX(1.4)";
+      }
     },
 
     expandNode(tree) {
       this.$emit("expandNode", tree);
+    },
+
+    iconExpandStyle(tree) {
+      return tree.expand ? `transform: ${this.inconDownStyle}` : "";
     },
   },
 
@@ -67,5 +101,20 @@ export default {
   user-select: none;
   font-family: Menlo;
   font-size: 12px;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+}
+
+.icon-translate {
+  margin-right: 3px;
+}
+
+.icon-rotate {
+  transform: scaleY(1.2) scaleX(1.4);
+  padding-bottom: 3px;
+  color: #c0c4cc;
 }
 </style>
