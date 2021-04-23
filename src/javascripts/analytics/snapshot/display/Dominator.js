52c230cf2a1733b54aa4a9c6f0d15c236964f6df
hyj1991 <yeekwanvong@gmail.com>
2021-04-23 15:31:48 +0800

    src: add dominators

--- src/javascripts/analytics/snapshot/display/Dominator.js
+++ src/javascripts/analytics/snapshot/display/Dominator.js
@@ -3,6 +3,7 @@
 import treeHelper from "@/javascripts/analytics/snapshot/display/TreeHelper";
 
 const { methods: treeMethods } = treeHelper;
+const dominatorMap = {};
 
 export default {
   created() {
@@ -11,7 +12,47 @@ export default {
 
   methods: {
     initTree() {
+      const { rootIndex } = this.profile;
+      const rootInfo = this.formatNode(rootIndex);
+      const { children, lastIndex, more, left, noChild } = this.formatEdges(rootIndex);
 
+      this.profileTreeData = [{
+        id: rootIndex,
+        title: rootInfo,
+        expand: true,
+        children, lastIndex, more,
+        left, noChild,
+        parents: [rootIndex]
+      }];
+    },
+
+    formatEdges(id, start = 0, parents = [], interval = 50) {
+      const childs = this.getSortedDominators(id);
+      const lastIndex = Math.min(childs.length, start + interval);
+      const children = [];
+      for (let index = start; index < lastIndex; index++) {
+        const targetNode = childs[index];
+        children.push({
+          id: targetNode,
+          title: this.formatNode(targetNode, null, parents),
+          parents: [targetNode].concat(parents),
+          disabled: parents.includes(targetNode),
+        });
+      }
+      return { children, lastIndex, more: lastIndex < childs.length, left: childs.length - lastIndex, noChild: !childs.length };
+    },
+
+    getSortedDominators(id) {
+      if (dominatorMap[id]) {
+        return dominatorMap[id];
+      }
+
+      const { dominators, retainedSizes } = this.profile;
+      const doms = dominators[id] || [];
+      doms.sort((o, n) => retainedSizes[o] < retainedSizes[n] ? 1 : -1);
+      dominatorMap[id] = doms;
+
+      return doms;
     },
 
     ...treeMethods
--- src/javascripts/analytics/snapshot/display/TreeHelper.js
+++ src/javascripts/analytics/snapshot/display/TreeHelper.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import * as utils from "@/javascripts/lib/utils";
+
 export default {
   methods: {
     expandNode(tree) {
@@ -17,6 +19,61 @@ export default {
       this.$set(tree, "more", more);
       this.$set(tree, "left", left);
       this.$set(tree, "noChild", noChild);
+    },
+
+    formatNode(id, edge, parents) {
+      const { nodeUtils, edgeUtils, retainedSizes, gcrootsMap, NodeUtils, EdgeUtils } = this.profile;
+
+      // node info
+      const { KCLOSURE, KSTRING, KCONCATENATED_STRING, KSLICED_STRING } = NodeUtils.NodeTypes;
+      const address = `<span class="snap-addr">@${nodeUtils.getAddress(id)}</span>`;
+      const type = nodeUtils.getType(id);
+      const nodeType = nodeUtils.getTypeForInt(id);
+      const size = utils.formatSize(retainedSizes[id], 2, false, false, " ");
+      let name = utils.htmlEscape(nodeUtils.getName(id));
+      if (nodeType === KCONCATENATED_STRING) {
+        name = nodeUtils.getConsStringName(id);
+      }
+      if (typeof name === "string" && name.length > 100) {
+        name = name.substr(0, 100);
+      }
+
+      let nameClass = ["snap-name"];
+      if ([KCLOSURE].includes(nodeType)) {
+        name = `${name || "anonymous"}()`;
+        nameClass.push("snap-closure");
+      }
+      if ([KSTRING, KSLICED_STRING, KCONCATENATED_STRING].includes(nodeType)) {
+        nameClass.push("snap-string");
+      }
+      if (gcrootsMap[id] && name) {
+        nameClass.push("snap-gcroot");
+      }
+      name = `<span class="${nameClass.join(" ")}">${name}</span>`;
+      let info = `${name} ${address} <span class="snap-detial">(type: ${type}, size: ${size})</span>`;
+
+      // edge info
+      if (edge || edge === 0) {
+        const nameOrIndex = utils.htmlEscape(edgeUtils.getNameOrIndex(edge, true));
+        const type = edgeUtils.GetType(edge, true);
+        const edgeType = edgeUtils.getTypeForInt(edge, true);
+        const { KELEMENT, KPROPERTY, KSHORTCUT, KCONTEXTVARIABLE } = EdgeUtils.EdgeTypes;
+        let prot = `<span class="snap-hidden">${nameOrIndex}</span>`;
+        if ([KELEMENT, KPROPERTY, KSHORTCUT].includes(edgeType)) {
+          prot = `<span class="snap-property">${nameOrIndex}</span>`;
+        }
+        if ([KCONTEXTVARIABLE].includes(edgeType)) {
+          prot = `<span class="snap-context">${nameOrIndex}</span>`;
+        }
+        info = `<span title="${type}">${prot}</title> <span class="snap-quto">::</span> ${info}`;
+      }
+
+      // if disabled
+      if (parents && parents.includes(id)) {
+        info = `<span class="snap-disabled">${info}</span>`;
+      }
+
+      return info;
     },
   }
 };
