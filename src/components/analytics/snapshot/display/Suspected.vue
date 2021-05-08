405767ff3a8af320b44da44ce20af46e01919bb8
hyj1991 <yeekwanvong@gmail.com>
2021-05-08 16:54:12 +0800

    src: add suspected leak nodes

--- src/components/analytics/snapshot/display/Suspected.vue
+++ src/components/analytics/snapshot/display/Suspected.vue
@@ -12,18 +12,40 @@
           <div class="card-content">
             <!-- single node -->
             <div v-if="leak.count === 1">
-              实例 <strong>"{{ leak.name }}"</strong> 地址
-              <strong>@{{ leak.address }}</strong> 占用了 V8 堆内
-              <strong>{{ leak.formatedSize }} ({{ leak.percentage }}%)</strong>
-              的空间.
+              <div class="content-interval">
+                实例 <strong>"{{ leak.name }}"</strong> 地址
+                <strong
+                  class="leak-address"
+                  :ref="`address-${index}`"
+                  @click="copyAddress(index)"
+                  >@{{ leak.address }}</strong
+                >
+                占用了 V8 堆内
+                <strong
+                  >{{ leak.formatedSize }} ({{ leak.percentage }}%)</strong
+                >
+                的空间.
+              </div>
+              <div class="card-title">关键信息</div>
+              <div class="card-content">
+                实例类型 <strong>{{ leak.type }},</strong> 总计
+                <strong>{{ leak.edgeCount }}</strong> 个边.
+              </div>
             </div>
 
             <!-- muliti nodes -->
             <div v-else>
-              <strong>{{ leak.count }}</strong> 个
-              <strong>"{{ leak.name }}"</strong> 对象占用了 V8 堆内
-              <strong>{{ leak.formatedSize }} ({{ leak.percentage }}%)</strong>
-              的空间.
+              <div class="content-interval">
+                <strong>{{ leak.count }}</strong> 个
+                <strong class="leak-address">"{{ leak.name }}"</strong>
+                对象占用了 V8 堆内
+                <strong
+                  >{{ leak.formatedSize }} ({{ leak.percentage }}%)</strong
+                >
+                的空间.
+              </div>
+              <div class="card-title">关键词</div>
+              <div class="card-content">{{ leak.name }}</div>
             </div>
           </div>
         </div>
@@ -57,7 +79,7 @@ export default {
 .leak-card {
   display: flex;
   background-color: #f8fafc;
-  border-left: 9px solid #2376b7;
+  border-left: 7px solid #9b59b6;
   padding: 10px 15px;
 }
 
@@ -72,5 +94,15 @@ export default {
 .card-content {
   margin-top: 7px;
   font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+}
+
+.leak-address {
+  user-select: none;
+  cursor: pointer;
+  color: #c45a65;
+}
+
+.content-interval {
+  margin-bottom: 13px;
 }
 </style>
